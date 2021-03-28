81fc966814c46bf8dae60fad853c0a2f51fb0ac0
Leandro Videla <leandrovidelamiranda@gmail.com>
2021-03-28 15:55:00 -0300

    gracias

--- dist/templates/hero/hero.php
+++ dist/templates/hero/hero.php
@@ -1,14 +1,6 @@
 <div class="Hero" id="hero">
   <div class="container">
     <div class="row">
-      <div class="col Hero__wrapper-title">
-        <div>
-          <div class="lottie lottie-1" data-animation-path="assets/img/promo/hero-4.json" data-anim-loop="true"
-            data-anim-type="svg" data-name="ninja"></div>
-        </div>
-      </div>
-    </div>
-    <div class="row">
       <div class="col text-center">
         <img src="assets/img/promo/hero.png" alt="" class="header-img-1 img-fluid">
       </div>
